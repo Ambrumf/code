f1abf561f600932aa8d94c90f915563003058f68
Ambrumf <421653227@qq.com>
2021-02-22 08:39:24 +0800

    v2(ac)

--- ACM/CF_ABC/N.cpp
+++ ACM/CF_ABC/N.cpp
@@ -2,34 +2,32 @@
 using namespace std;
 typedef long long ll;
 #define inf 0x3f3f3f3f;
-
+ll gcd(ll a,ll b){
+    if(a<b)swap(a,b);
+    return b?gcd(b,a%b):a;
+}
+double lcm(ll a,ll b){
+    if(a<b)swap(a,b);
+    return (double)a/gcd(a,b)*b;
+} 
 int main()
 {
-    ll n,m,p;
+    ll n,m,p,a,b;
     cin>>n>>m>>p;
+    ll num=gcd(n,m);
+    a=n/num;b=m/num;
+    ll x1,y1,x2,y2;
     while(p--){
-        ll x1,y1,x2,y2;
-        cin>>x1>>y1>>x2>>y1;
-        {
-            if(x1==x2){
-                if(y2-y1==1 || y1-y2==1 || (x1==1 && (y1==1 && y2==n || y1==n && y2==1) ) || (x1==2 && (y1==1 && y2==m || y1==m &&y2==1) ))cout<<"yes"<<endl;
-                else cout<<"no"<<endl;
-            }
-            else {
-                double l1,r1,l2,r2;
-                if(x1==1){
-                    l1=(y1-1)/n;r1=y1/n;}
-                else {
-                    l1=(y1-1)/m;r1=y1/m;
-                }
-                if(x2==1){
-                    l2=(y2-1)/n;r2=y2/n;}
-                else {
-                    l2=(y2-1)/m;r2=y2/m;}
-                if(l1<r2 && l2<r1)cout<<"yes"<<endl;
-                else cout<<"no"<<endl;
-            }
-        }   
+    cin>>x1>>y1>>x2>>y2;
+    y1--;y2--;
+    ll p1,p2;
+    if(x1==1)p1=(y1/a);
+    else p1=(y1/b);
+    if(x2==1)p2=(y2/a);
+    else p2=(y2/b);
+    //cout<<p1<<' '<<p2<<endl;
+    if(p1==p2)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
     }
     return 0;
 }
