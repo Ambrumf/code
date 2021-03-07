f733bc18a31e144f150e1e988a3240f24f1e6794
ambrumf <421653227@qq.com>
2021-03-07 18:54:35 +0800

    code 03.07

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define inf 0x3f3f3f3f;
+
+void solve(){
+    ll a,b;
+    cin>>a>>b;
+    ll cnt=0;
+    for(ll i=b;i*i<a;i++){
+        if(a%i==0)cnt++;
+    }
+    cout<<cnt<<endl;
+}
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=1;i<=t;i++){
+        cout<<"Case "<<i<<": ";
+        solve();
+    }
+    return 0;
+}
---
+++ test2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    freopen("test.cpp","r",stdin);
+    string t;
+    while(getline(t)){
+        cout<<t<<endl;
+    }    
+    return 0;
+}
